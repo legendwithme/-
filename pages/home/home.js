710a76e08957dfc2252e6728bead5addb5206d4e
legendwithme <53105673+legendwithme@users.noreply.github.com>
2019-07-21 12:00:39 +0800

    20190721test1

---
+++ pages/home/home.js
@@ -0,0 +1,13 @@
+Page({
+  data: {
+  indicatorDots:true,
+  autoplay:true,
+  interval:4000,
+  duration:1000,
+  imgUrls:[
+  '../images/homeTitle/1.jpg',
+  '../images/homeTitle/2.jpg',
+  '../images/homeTitle/3.jpg',
+  ]
+  }
+})
